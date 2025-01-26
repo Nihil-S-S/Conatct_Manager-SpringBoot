51b138d3ae582e8923d7c69d90e846947deaf74a
Nihil-S-S <nihilss2000@gmail.com>
2025-01-26 11:22:45 +0530

    spring security hard coded

--- src/main/java/com/Contact_Manager/contact_manager/config/SecurityConfig.java
+++ src/main/java/com/Contact_Manager/contact_manager/config/SecurityConfig.java
@@ -3,18 +3,23 @@ package com.Contact_Manager.contact_manager.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()) // Disable CSRF for the application
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // Allow all requests
-                );
-        return http.build();
+    public UserDetailsService userDetailsService(){
+
+        UserDetails user1 = User.withDefaultPasswordEncoder().username("nihil").password("300971").build();
+
+        var inMemoryUserDetailsManager = new InMemoryUserDetailsManager(user1);
+        return inMemoryUserDetailsManager;
     }
 }
 
