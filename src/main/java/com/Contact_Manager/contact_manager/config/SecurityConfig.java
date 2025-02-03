cefeb5c96466cc75e956775deaf05a5d87fddb35
Nihil-S-S <nihilss2000@gmail.com>
2025-02-03 22:04:22 +0530

    google oauth setup

--- src/main/java/com/Contact_Manager/contact_manager/config/SecurityConfig.java
+++ src/main/java/com/Contact_Manager/contact_manager/config/SecurityConfig.java
@@ -14,10 +14,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.webauthn.api.PublicKeyCose;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Autowired
